98c65fac91318e68c6cbb626e7f9486c5c32b6be
imadeddinekebour <imadeddinekebourlite@gmail.com>
2022-08-03 21:06:55 +0100

    fixing errors

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const {Server} = require('socket.io')
 
 const io= new Server(server,{
     cors:{
-        origin:"http://localhost:3000",
+        origin:"https://62ead40efed68b0008a990ea--warm-cannoli-04961c.netlify.app",
         methodes:["GET","POST"],
     }
 })
